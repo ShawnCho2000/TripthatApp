7f6e3196c619f81c7ce295b7ff9e8dbe46cee255
Shawn Cho <shawn2000@outlook.com>
2024-01-13 19:08:13 -0800

    Initial Commit

---
+++ TripthatApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TripthatApp
+//
+//  Created by Shawn Cho on 1/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripthatApp/TripthatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripthatAppApp.swift
+//  TripthatApp
+//
+//  Created by Shawn Cho on 1/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TripthatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
